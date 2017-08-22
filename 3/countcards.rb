f0e522f862aefb0c2f1a4215b11a3c996b2b9a63
Andy Tran <andy8tran@gmail.com>
2017-08-22 09:24:39 -0400

    added casino cheat scenario

--- 3/countcards.rb
+++ 3/countcards.rb
@@ -1,10 +1,22 @@
 def card_count(string_of_cards)
 
   count = 0
+  each_count = {}
 
   return false if string_of_cards.length > 52
 
   string_of_cards.split("").each do |each_card|
+
+    if each_count[each_card] == nil   #if it's not yet in each_count,
+      each_count[each_card] = 1       #set count to 1
+    elsif each_count[each_card] == 4  #if count for this card is arleady 4, it's a cheat!
+      return false
+    else
+        each_count[each_card] += 1    #otherwise, increase the count by 1
+    end
+
+
+
     case each_card.to_i
     when 0
       count -= 1
@@ -22,3 +34,4 @@ end
 puts card_count('K2T6AA')
 puts card_count('798TT23')
 puts card_count('235T2')
+puts card_count('KKT2KKK')
