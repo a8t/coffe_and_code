2352650a1148ef924fcc02c7bfa9051485cd3322
Andy Tran <andy8tran@gmail.com>
2017-08-31 14:27:39 -0400

    added comments

--- 6/leapyears.rb
+++ 6/leapyears.rb
@@ -7,7 +7,7 @@ def find_leap_years(start_year, end_year)
   #increment until we're looking at the first leap year in the range
   current_year += 1 until current_year % 4 == 0
 
-  until current_year > end_year do
+  until current_year > end_year do #stop once we're past end_year
 
     # skip the exception years
     unless current_year % 100 == 0 && current_year % 400 != 0
@@ -22,5 +22,4 @@ def find_leap_years(start_year, end_year)
 
 end
 
-p find_leap_years(2000, 2012)
-p find_leap_years(1895, 1910)
+# 500000.times do find_leap_years(2000, 2012) end (did this to time my algorithm)
