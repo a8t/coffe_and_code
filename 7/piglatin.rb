aba169b21d49d61f33b0e63b8507fabc409d6ba8
Andy Tran <andy8tran@gmail.com>
2017-09-05 10:07:06 -0400

    cleaned piglatin

--- 7/piglatin.rb
+++ 7/piglatin.rb
@@ -1,21 +1,12 @@
-# apple => appleay
-#  => ananabay
-#  => awberrystray
-#  => uietqay
-#  => eatay iepay
-#
-
 def pig(string)
 
   words_array = string.split(" ")
 
   words_array.map do |each_word|
     i = each_word.index(/[aeiou]/)
-
     new_word = each_word[i..-1] + each_word[0...i] + "ay"
-
   end
-  
+
 end
 
 puts pig("apple")
