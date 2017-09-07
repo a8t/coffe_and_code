f2e30191017279d5efabd44176337c4f4de6ff3f
Andy Tran <andy8tran@gmail.com>
2017-09-07 10:06:18 -0400

    formatting

--- 8/narcissistic.rb
+++ 8/narcissistic.rb
@@ -1,27 +1,3 @@
-# A Narcissistic Number is a number where the sum of its own digits, each raised to the power of the number of digits, IS the number itself. (because it looks at itself a lot, ya dig?)
-#
-# For example, take 153 (3 digits): 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-#
-# and 1634 (4 digits): 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
-#
-# The Challenge: Your function must return true or false depending upon whether the given number is a Narcissistic number. You can assume that your function will only be passed valid integers, so no input or error-checking is required.
-#
-# Hints
-#
-# # The "to the power of" operator in ruby is two asterisks, so
-# 3 ** 4 = 81
-# # An easy way to split an integer into digits is to make it a string first... Then maybe you could 'split' it?
-# 1634.to_s...
-# def narcissistic(number)
-#   # your code here!
-# end
-#
-# #example usage
-# narcissistic(153)  #true
-# narcissistic(1634) # true
-# narcissistic(1635) # false
-
-
 def intiger_to_arr_of_digits(num)
   # converts number to string, splits string to array of characters,
   # and then converts each character to integer
@@ -34,7 +10,6 @@ def narcissistic(number)
   digit_array = integer_to_arr_of_digits(number)
 
   digit_count = digit_array.count
-
 
   # returns new array (called raised_array) where each element of
   # digit_array has been raised to digit_count
