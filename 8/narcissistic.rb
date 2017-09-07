19a5eb629ea35cde55db2d5a51ca2eb3d1b1e838
Andy Tran <andy8tran@gmail.com>
2017-09-07 10:00:33 -0400

    commented narccissistic

--- 8/narcissistic.rb
+++ 8/narcissistic.rb
@@ -22,27 +22,31 @@
 # narcissistic(1635) # false
 
 
+def intiger_to_arr_of_digits(num)
+  # converts number to string, splits string to array of characters,
+  # and then converts each character to integer
+  num.to_s.split("").map { |e| e.to_i }
+
+end
+
 def narcissistic(number)
 
-  digit_array = num_to_arr_of_digits(number)
+  digit_array = integer_to_arr_of_digits(number)
 
   digit_count = digit_array.count
 
+
+  # returns new array (called raised_array) where each element of
+  # digit_array has been raised to digit_count
   raised_array = digit_array.map do |each|
     each = each ** digit_count
   end
 
-
   number == raised_array.sum
 
-
 end
 
-def num_to_arr_of_digits(num)
 
-  num.to_s.split("").map { |e| e.to_i }
-
-end
 
 puts narcissistic(153)  #true
 puts
