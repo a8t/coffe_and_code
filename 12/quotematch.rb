94ca61f73f8e0ef6b024685f80e322448c584bf8
Andy Tran <andy8tran@gmail.com>
2017-09-21 09:34:21 -0400

    checkquotes

---
+++ 12/quotematch.rb
@@ -0,0 +1,42 @@
+# Problems for coffee & code Session 12
+#
+# Matching Quotation Marks (together as a group)
+#
+# Write a method that accepts a string as an argument and returns true or false indicating whether all quotations within that string are properly closed.
+#
+# For example:
+#
+# check_quotes('"Hello" said Bell \n "How are you?" replied Kat') # => true
+# check_quotes('"Hello said Bell \n "How are you?" replied Kat') # => false
+# Incorporate with Bracket-Matching Logic (individual)
+#
+# Return to the bracket-matching code from this past reinforcement exercise and integrate the code we wrote together so your method checks brackets ((), [], {}, <>) as well as quotes (" ").
+
+
+def check_quotes(quoteString)
+
+  even_number = quoteString.count('"') % 2 == 0
+
+  if even_number
+    quoteString[quoteString.index('"')] = ''
+
+    if quoteString.index('"')
+      if quoteString.index('\n') < quoteString.index('"')
+        return false
+      else
+        quoteString[quoteString.index('"')] = ''
+        quoteString[quoteString.index('\n')] = ''
+      end
+    end
+    
+    return true
+
+  else
+    false
+  end
+
+
+end
+
+p check_quotes('"Hello" said Bell \n "How are you?" replied Kat. \n "I am doing okay," said Bell.') # => true
+p check_quotes('"Hello said Bell \n "How are you?" replied Kat') # => false
